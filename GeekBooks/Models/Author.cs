a140f4536042135fd2839b12c3baebe6f27e7ef7
Alex <Alex@DESKTOP-NKIQ48L>
2017-06-12 21:05:09 -0400

    author added

---
+++ GeekBooks/Models/Author.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekBooks.Models
+{
+    public class Author
+    {
+
+        [Key]
+        public int AuthID { get; set; }
+
+        [StringLength(255)]
+        [Required(ErrorMessage = "Author Name is Reqiured")]
+        public string AuthorName { get; set; }
+
+        [StringLength(2000)]
+        public string Blurb { get; set; }
+    }
+}
