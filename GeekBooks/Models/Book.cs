c10bb37bb0805e7e53659502ea81c4188b70f057
Alex <Alex@DESKTOP-NKIQ48L>
2017-07-24 06:37:06 -0400

    edits to pagination so that a max of 5 page tags can appear at any time

--- GeekBooks/Models/Book.cs
+++ GeekBooks/Models/Book.cs
@@ -16,7 +16,7 @@ namespace GeekBooks.Models
         public int AuthorID { get; set; }
 
         [Display(Name ="Book Name")]
-        [StringLength(255)]
+        [StringLength(256)]
         public string BookName { get; set; }
 
         [Display(Name = "Cover Image")]
